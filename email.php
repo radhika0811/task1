290ff15c483b9b0dbe656772845d55e54465a308
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2020-04-13 19:21:46 +0530

    Update email.php

--- email.php
+++ email.php
@@ -7,8 +7,8 @@ echo $theData;
 if (empty($theData)) {
     echo "Please enter values in fields";
 } else {
-    ini_set("SMTP", "localhost");
-    ini_set("smtp_port", "25");
+    ini_set("SMTP", "smtp.gmail.com");
+    ini_set("smtp_port", "587");
     ini_set("sendmail_from", "radhikaa245@gmail.com");
     ini_set("sendmail_path", "C:/xampp/sendmail/sendmail.exe");
 
