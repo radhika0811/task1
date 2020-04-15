ef40fbbc7fcf1a9ac7965763bb1fcaa9ca0743fb
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2020-04-15 16:52:52 +0530

    Update email.php

--- email.php
+++ email.php
@@ -9,8 +9,9 @@ $password = $_REQUEST['password'];
 $month = $_REQUEST['month'];
 $day = $_REQUEST['day'];
 $year = $_REQUEST['year'];
-echo json_encode($_GET);
-if (empty($firstname) || empty($lastname) || empty($email)  || empty($address) || empty($password)) {
+$amount = $_REQUEST['amount'];
+//echo json_encode($_GET);
+if (empty($firstname) || empty($lastname) || empty($email)  || empty($address) || empty($password) || empty($amount)) {
     echo "Please enter values in fields";
 } /*if (empty($theData)) {
     echo "Please enter values in fields";
@@ -20,7 +21,7 @@ if (empty($firstname) || empty($lastname) || empty($email)  || empty($address) |
     ini_set("sendmail_from", "radhikaa245@gmail.com");
     ini_set("sendmail_path", "C:/xampp/sendmail/sendmail.exe");
     //mail("radhikaa245@gmail.com", "Form Submission Details", "From: $theData\n");
-    if (mail("radhikaa245@gmail.com", "Form Submission Details", "From: $firstname $lastname $month $day $year $age $address\n", "Yes")) echo "mail sent";
+    if (mail("Jatin.ibs@gmail.com", "Form Submission Details", "From: $firstname $lastname $month $day $year $age $address $amount\n", "Yes")) echo "mail sent";
     else echo "error sending mail";
 }
 //fclose($fm);
