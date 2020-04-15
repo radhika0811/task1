d28b4a3d97fad936fa88a67416dc6b18a76b0307
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2020-04-15 16:37:08 +0530

    Update email.php

--- email.php
+++ email.php
@@ -1,12 +1,16 @@
 <?php
+
 $firstname = $_REQUEST['fname'];
 $lastname = $_REQUEST['lname'];
 $age = $_REQUEST['age'];
-$email = $_REQUEST['mail'];
+$email = $_REQUEST['email'];
 $address = $_REQUEST['address'];
 $password = $_REQUEST['password'];
-
-if (empty($firstname) || empty($lastname) || empty($email) || empty($address) || empty($password)) {
+$month = $_REQUEST['month'];
+$day = $_REQUEST['day'];
+$year = $_REQUEST['year'];
+echo json_encode($_GET);
+if (empty($firstname) || empty($lastname) || empty($email)  || empty($address) || empty($password)) {
     echo "Please enter values in fields";
 } /*if (empty($theData)) {
     echo "Please enter values in fields";
@@ -15,10 +19,10 @@ if (empty($firstname) || empty($lastname) || empty($email) || empty($address) ||
     ini_set("smtp_port", "587");
     ini_set("sendmail_from", "radhikaa245@gmail.com");
     ini_set("sendmail_path", "C:/xampp/sendmail/sendmail.exe");
-
     //mail("radhikaa245@gmail.com", "Form Submission Details", "From: $theData\n");
-    if (mail("radhikaa245@gmail.com", "Form Submission Details", "From: $firstname $lastname $email $age $address\n", "Yes")) echo "mail sent";
+    if (mail("radhikaa245@gmail.com", "Form Submission Details", "From: $firstname $lastname $month $day $year $age $address\n", "Yes")) echo "mail sent";
     else echo "error sending mail";
 }
 //fclose($fm);
 ?>
+
