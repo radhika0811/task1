519e246280221d8cfd18aa493f4e1d87cb095355
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2020-04-27 23:47:01 +0530

    Update email.php

--- email.php
+++ email.php
@@ -11,7 +11,7 @@ $uploadStatus = '';
 if(!empty($_FILES["file"]["name"])){
     $targetDir = "uploads/";
     $fileName = basename($_FILES["file"]["name"]);
-    $targetFilePath = $targetDir . $fileName;
+    $targetFilePath = $targetDir.$fileName;
     $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
     $allowTypes = array('pdf', 'doc', 'jpg', 'png', 'jpeg');
 }
@@ -20,11 +20,11 @@ if(in_array($fileType, $allowTypes)){
     if(move_uploaded_file($_FILES["attachment"]["tmp_name"], $targetFilePath)){
         $uploadedFile = $targetFilePath;
     }else{
-        $uploadStatus = 0;
+       // $uploadStatus = 0;
         $statusMsg = "Sorry, there was an error uploading your file.";
     }
 }else{
-    $uploadStatus = 0;
+   // $uploadStatus = 0;
     $statusMsg = 'Sorry, only PDF, DOC, JPG, JPEG, & PNG files are allowed to upload.';
 }
 $pincode = $_REQUEST['zip'];
@@ -41,7 +41,7 @@ if (empty($firstname) || empty($email)  || empty($address1) || empty($address2) 
     ini_set("sendmail_path", "C:/xampp/sendmail/sendmail.exe");
     $toEmail = "Jatin.ibs@gmail.com" ;
     $from = $email;
-    $emailSubject = 'Contact Request Submitted by '.$firstname;
+    $emailSubject = 'A  new order by '.$firstname;
     $htmlContent = '
 
 
@@ -55,49 +55,39 @@ Mobile Number: '.$mobno.'
 Pincode: '.$pincode.'
 ';
     $headers = "From: $firstname"." <".$from.">";
-    if(!empty($uploadedFile) && file_exists($uploadedFile)){
-// Boundary
-        $semi_rand = md5(time());
-        $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
-// Headers for attachment
-        $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";
-// Multipart boundary
-        $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
-            "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";
-// Preparing attachment
-        if(is_file($uploadedFile)){
+    $semi_rand = md5(time());
+    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+
+// Headers for attachment  
+    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";
+
+// Multipart boundary  
+    $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
+        "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";
+
+// Preparing attachment 
+    if(!empty($file) > 0){
+        if(is_file($fileName)){
             $message .= "--{$mime_boundary}\n";
-            $fp = @fopen($uploadedFile,"rb");
-            $data = @fread($fp,filesize($uploadedFile));
+            $fp =    @fopen($fileName,"rb");
+            $data =  @fread($fp,filesize($fileName));
+
             @fclose($fp);
             $data = chunk_split(base64_encode($data));
-            $message .= "Content-Type: application/octet-stream; name=\"".basename($uploadedFile)."\"\n" .
-                "Content-Description: ".basename($uploadedFile)."\n" .
-                "Content-Disposition: attachment;\n" . " filename=\"".basename($uploadedFile)."\"; size=".filesize($uploadedFile).";\n" .
+            $message .= "Content-Type: application/octet-stream; name=\"".basename($fileName)."\"\n" .
+                "Content-Description: ".basename($fileName)."\n" .
+                "Content-Disposition: attachment;\n" . " filename=\"".basename($fileName)."\"; size=".filesize($fileName).";\n" .
                 "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
         }
-        $message .= "--{$mime_boundary}--";
-        $returnpath = "-f" . $email;
-// Send email
-        $mail = mail($toEmail, $emailSubject, $message, $headers, $returnpath);
-// Delete attachment file from the server
-        @unlink($uploadedFile);
     }
-    else{
-// Set content-type header for sending HTML email
-        $headers .= "\r\n". "MIME-Version: 1.0";
-        $headers .= "\r\n". "Content-type:text/html;charset=UTF-8";
-// Send email
-        $mail = mail($toEmail, $emailSubject, $htmlContent, $headers);
-    }
-// If mail sent
-    if($mail){
-        $statusMsg = 'Your contact request has been submitted successfully !';
-        $msgClass = 'succdiv';
-        $postData = '';
-    }else{
-        $statusMsg = 'Your contact request submission failed, please try again.';
-    }
+    $message .= "--{$mime_boundary}--";
+    $returnpath = "-f" . $from;
+
+// Send email 
+    $mail = @mail($toEmail, $emailSubject, $message, $headers, $returnpath);
+
+// Email sending status 
+    echo $mail?"<h1>Your Application Will Be Processed Soon!</h1>":"<h1>Please Try Again.</h1>";
 }
 //fclose($fm);
 ?>
