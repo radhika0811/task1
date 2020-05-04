91f07f934392f53a7755a6328faf1764ae1117d8
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2020-05-05 01:40:53 +0530

    Update email.php

--- email.php
+++ email.php
@@ -80,12 +80,20 @@ Pincode: '.$pincode.'
     }
     $message .= "--{$mime_boundary}--";
     $returnpath = "-f" . $from;
-
+echo $fileName;
+echo json_encode($_POST);
 // Send email
     $mail = mail($toEmail, $emailSubject, $message, $headers, $returnpath);
-
 // Email sending status
     echo $mail?"<h1>Email Sent Successfully!</h1>":"<h1>Email sending failed.</h1>";
+}
+if($mail){
+    Windows.history.log(-1);
+    alert("Thanks for submitting your details. Our team will contact you soon");
+}
+else
+{
+    alert("Try again later");
 }
 //fclose($fm);
 ?>
